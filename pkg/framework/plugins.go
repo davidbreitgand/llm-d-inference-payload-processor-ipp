7ef11d7a6c5d54bb4b9c9ce47420249a8438c75e
David Breitgand <davidbreitgand@users.noreply.github.com>
2026-05-10 20:22:48 +0300

    Adds Scorer interface
    Signed-off-by: David Breitgand <davidbreitgand@users.noreply.github.com>

--- pkg/framework/plugins.go
+++ pkg/framework/plugins.go
@@ -18,6 +18,30 @@ package framework
 
 import (
 	"context"
+
+	"github.com/llm-d/llm-d-inference-payload-processor/pkg/framework/datalayer"
+)
+
+// ScorerCategory indicates the preference a scorer applies when scoring candidate models
+type ScorerCategory string
+
+const (
+	// Cheapest indicates a scorer that prefers models with the lowest USD/(1M tokens) price
+	Cheapest ScorerCategory = "Cheapest"
+
+	// TODO: Add other cost-related scorer categories here after the simplest "Cheapest" category scorer is implemented
+	// Value indicates a scorer that prefers models sitting on the efficiency curve
+	// A model is better than another model only if it is both more accurate and cheaper
+	// Value = (normalized accuracy rank)/(actual total cost)
+	// Value ScorerCategory = "Value"
+
+	// PredictiveCost indicates a scorer that prefers models for which predicted cost is lowest
+	// PredictiveCost ScorerCategory = "PredictiveCost"
+
+	// PredictiveValue indicates a scorer that prefers models for which predicted value is highest
+	// PredictiveValue ScorerCategory = "PredictiveValue"
+
+	// TODO: Add other scorer categories here for other types of scoring
 )
 
 // Plugin defines the interface for a plugin.
@@ -39,4 +63,14 @@ type ResponseProcessor interface {
 	// ProcessResponse runs the ResponseProcessor plugin.
 	// ResponseProcessor can mutate the headers and/or the body of the response.
 	ProcessResponse(ctx context.Context, cycleState *CycleState, response *InferenceResponse) error
+}
+
+// Scorer defines the interface for a scorer plugin that scores a list of models based on the category and context.
+// Scorer implementation MUST return a score in [0,1] with 1 being the highest score
+type Scorer interface {
+	Plugin
+	// Category returns the category of the scorer
+	Category() ScorerCategory
+	// Score returns a score for each input model
+	Score(ctx context.Context, cycleState *CycleState, models []*datalayer.Model) map[string]float64
 }
